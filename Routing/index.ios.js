b7a2fe582d4cceddc980114b71a10ecbdf29a161
angularyuni <angularyuni@users.noreply.github.com>
2017-04-28 19:31:40 -0400

    For mac - should be test later

--- Routing/index.ios.js
+++ Routing/index.ios.js
@@ -1,53 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Navigator
 } from 'react-native';
 
+import Home from './components/Home';
+import About from './components/About';
+
 export default class Routing extends Component {
+    
+  constructor() {
+    super()
+    this.renderScene = this.renderScene.bind(this)
+  }
+
+  renderScene(route, navigator) {
+    if(route.name === 'homePage') {
+      return <Home navigator={navigator} />
+    } else if(route.name === 'aboutPage') {
+      return <About navigator={navigator} text={route.text} />
+    }
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+     <Navigator
+        initialRoute = {{name: 'homePage'}}
+        renderScene = {this.renderScene}
+     />
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('Routing', () => Routing);
